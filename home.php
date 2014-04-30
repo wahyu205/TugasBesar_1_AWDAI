3a892e1cb57a14480842e00e727d790037e65519
Andre Rizkiana Wandira <yuuzuzero@gmail.com>
2014-04-30 14:46:05 +0700

    penambahan file interface
    file untuk menampilkan halaman awal.

---
+++ home.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<style>
+p.padding
+{
+padding-top:25px;
+padding-bottom:25px;
+padding-right:50px;
+padding-left:50px;
+color:#612915;
+}
+</style>
+</head>
+<body>
+	  <p class="padding">
+	Selamat datang di Web kami. Web ini di susun untuk memenuhi Tugas Besar Layanan Web.<br />
+	117006202 Andre Rizkiana Wandira<br />
+	117006205 Wahyu Kartika Hadi<br />
+	117006206 Dede Zuliana<br />
+	117006215 Aditya Bayu Pradana<br />
+	117006219 Iis Uswatun Hasanah<br />
+	
+	  
+ </p>
+ </body>
+ </html>
