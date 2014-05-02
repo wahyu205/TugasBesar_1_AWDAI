8a57d8e24c28de35a8c225816f43ae99c28d30a1
Wahyu Kartika Hadi <wahzuzumy@gmail.com>
2014-05-02 15:39:59 +0700

    tambah codign font and size

--- home.php
+++ home.php
@@ -13,14 +13,16 @@ color:#612915;
 </head>
 <body>
 	  <p class="padding">
+	<font type="Blackkader ITC" size=16>
 	Selamat datang di Web kami. Web ini di susun untuk memenuhi Tugas Besar Layanan Web.<br />
+	</font>
+	<font type="monotype corsiva" size=12>
 	117006202 Andre Rizkiana Wandira<br />
 	117006205 Wahyu Kartika Hadi<br />
 	117006206 Dede Zuliana<br />
 	117006215 Aditya Bayu Pradana<br />
 	117006219 Iis Uswatun Hasanah<br />
-	
-	  
+	</font>
  </p>
  </body>
  </html>
